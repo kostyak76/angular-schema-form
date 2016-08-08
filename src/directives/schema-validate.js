075453d1e9cf2c9a8fe02af230884f3886b9eda8
kostyak76 <kostyak7676@gmail.com>
2016-08-08 13:20:42 +0300

    Changed `required` field resolution

--- src/directives/schema-validate.js
+++ src/directives/schema-validate.js
@@ -1,18 +1,5 @@
 angular.module('schemaForm').directive('schemaValidate', ['sfValidator', 'sfSelect', function(sfValidator, sfSelect) {
 
-  function guessDefaultValueForSchemaDefinition(schemaDefinition) {
-    if (schemaDefinition && schemaDefinition.type) {
-      switch (schemaDefinition.type) {
-        case 'string':
-          return "";
-        case 'number':
-          return "";
-        default:
-          return undefined;
-      }
-    }
-  }
-
   return {
     restrict: 'A',
     scope: false,
@@ -99,28 +86,28 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', 'sfSele
 
       // Listen to an event so we can validate the input on request
       scope.$on('schemaFormValidate', function() {
-        if (ngModel.$setDirty) {
-          // Angular 1.3+
-          var isDefaultValueSet = false;
-          if (!ngModel.$dirty && ngModel.$viewValue === undefined) {
-            var defaultValue = guessDefaultValueForSchemaDefinition(getForm().schema);
-            if (defaultValue !== undefined) {
-              ngModel.$setViewValue(defaultValue);
-              ngModel.$commitViewValue();
-              isDefaultValueSet = true;
-            }
-          }
-          ngModel.$setDirty();
-          ngModel.$$parseAndValidate();
-          if (isDefaultValueSet) {
-            ngModel.$setViewValue(defaultValue);
+        // We set the viewValue to trigger parsers,
+          // since modelValue might be empty and validating just that
+          // might change an existing error to a "required" error message.
+          if (ngModel.$setDirty) {
+
+            // Angular 1.3+
+            ngModel.$setDirty();
+            ngModel.$setViewValue(ngModel.$viewValue);
             ngModel.$commitViewValue();
+
+            // In Angular 1.3 setting undefined as a viewValue does not trigger parsers
+            // so we need to do a special required check. Fortunately we have $isEmpty
+            if (form.required && ngModel.$isEmpty(ngModel.$modelValue)) {
+              ngModel.$setValidity('tv4-302', false);
+            }
+
+          } else {
+            // Angular 1.2
+            // In angular 1.2 setting a viewValue of undefined will trigger the parser.
+            // hence required works.
+            ngModel.$setViewValue(ngModel.$viewValue);
           }
-          //validate(ngModel.$viewValue);
-        } else {
-          // Angular 1.2
-          ngModel.$setViewValue(ngModel.$viewValue);
-        }
       });
 
       scope.$on('schemaFormResetValidationFeedback', function() {
